babfe83189aa0cc85ce0b0cc1c5a3e1fb7124b4b
Griffithsl14 <griffithsl14@gmail.com>
2022-11-03 15:39:51 -0400

    initial commit

--- notes_main.py
+++ notes_main.py
@@ -1,2 +1,18 @@
-print("Hello World!")
-print("beeping!")
+# 2022-11-03 3:38 pm
+# This is like the 10th time im starting this because I could never get Git right
+# This project will be used to create a simple text/ journal app
+# As well as test out and get a better understanding of GitHub and version control!
+# Without further adieu, lets begin!
+
+def main():
+
+
+
+
+
+
+
+
+
+
+main()
